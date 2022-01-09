1a6cfd472b75199fd17bca2eed4fd450ba78cd07
Piotr Sienkiewicz <sienkiew@live.com>
2022-01-09 15:59:03 +0100

    ToString in Event

--- Models/Event.cs
+++ Models/Event.cs
@@ -6,5 +6,10 @@
         public string Description { get; set; } = "Description";
         public string Link { get; set; } = "Link";
         public string Guid { get; set; } = "Guid";
+
+        public override string ToString()
+        {
+            return $"Title: {Title}\nDescription: {Description}\nLink: {Link}\nGuid: {Guid}";
+        }
     }
 }
