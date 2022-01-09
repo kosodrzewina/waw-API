8e28af63ff011a1a3c9979a2fb2e4c144335c352
Piotr Sienkiewicz <sienkiew@live.com>
2022-01-09 21:08:16 +0100

    file-scoped namespaces

--- EventFetcher.cs
+++ EventFetcher.cs
@@ -1,72 +1,71 @@
 ﻿using System.Xml.Linq;
 using WawAPI.Models;
 
-namespace WawAPI
+namespace WawAPI;
+
+public class EventFetcher
 {
-    public class EventFetcher
+    private readonly Uri? _uri;
+
+    public EventFetcher(string address)
     {
-        private readonly Uri? _uri;
-
-        public EventFetcher(string address)
+        if (IsUrlValid(address))
         {
-            if (IsUrlValid(address))
-            {
-                _uri = new Uri(address);
-            }
-            else
-            {
-                _uri = null;
-                throw new Exception($"{address} is not a valid URL");
-            }
+            _uri = new Uri(address);
+        }
+        else
+        {
+            _uri = null;
+            throw new Exception($"{address} is not a valid URL");
+        }
+    }
+
+    public static bool IsUrlValid(string address)
+    {
+        return Uri.TryCreate(address, UriKind.Absolute, out Uri? uri)
+            && (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps);
+    }
+
+    public async Task<ICollection<Event>?> Fetch()
+    {
+        if (_uri == null)
+        {
+            return null;
         }
 
-        public static bool IsUrlValid(string address)
+        using var httpClient = new HttpClient();
+        var httpResponseMessage = await httpClient.GetAsync(_uri);
+
+        if (!httpResponseMessage.IsSuccessStatusCode)
         {
-            return Uri.TryCreate(address, UriKind.Absolute, out Uri? uri)
-                && (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps);
+            return null;
         }
 
-        public async Task<ICollection<Event>?> Fetch()
-        {
-            if (_uri == null)
-            {
-                return null;
-            }
-
-            using var httpClient = new HttpClient();
-            var httpResponseMessage = await httpClient.GetAsync(_uri);
-
-            if (!httpResponseMessage.IsSuccessStatusCode)
-            {
-                return null;
-            }
-
-            var feed = XDocument.Load(_uri.ToString());
+        var feed = XDocument.Load(_uri.ToString());
                 
-            return feed.Descendants()
-            .Where(item => item.Name == "item")
-            .Select(item => {
-                var title = item.Element("title");
-                var description = item.Element("description");
-                var link = item.Element("link");
-                var guid = item.Element("guid");
+        return feed.Descendants()
+        .Where(item => item.Name == "item")
+        .Select(item => {
+            var title = item.Element("title");
+            var description = item.Element("description");
+            var link = item.Element("link");
+            var guid = item.Element("guid");
 
-                return new Event
-                {
-                    Title = title != null && title.Value != null ?
-                        title.Value :
-                        "An error occurred when fetching title",
-                    Description = description != null && description.Value != null ?
-                        description.Value :
-                        "An error occurred when fetching description",
-                    Link = link != null && link.Value != null ?
-                        link.Value :
-                        "An error occurred when fetching link",
-                    Guid = guid != null && guid.Value != null ?
-                        guid.Value :
-                        "An error occurred when fetching guid"
-                };
-            }).ToList();
-        }
+            return new Event
+            {
+                Title = title != null && title.Value != null ?
+                    title.Value :
+                    "An error occurred when fetching title",
+                Description = description != null && description.Value != null ?
+                    description.Value :
+                    "An error occurred when fetching description",
+                Link = link != null && link.Value != null ?
+                    link.Value :
+                    "An error occurred when fetching link",
+                Guid = guid != null && guid.Value != null ?
+                    guid.Value :
+                    "An error occurred when fetching guid"
+            };
+        }).ToList();
     }
 }
--- Models/Event.cs
+++ Models/Event.cs
@@ -1,15 +1,14 @@
-﻿namespace WawAPI.Models
-{
-    public class Event
-    {
-        public string Title { get; set; } = "Title";
-        public string Description { get; set; } = "Description";
-        public string Link { get; set; } = "Link";
-        public string Guid { get; set; } = "Guid";
+﻿namespace WawAPI.Models;
 
-        public override string ToString()
-        {
-            return $"Title: {Title}\nDescription: {Description}\nLink: {Link}\nGuid: {Guid}";
-        }
+public class Event
+{
+    public string Title { get; set; } = "Title";
+    public string Description { get; set; } = "Description";
+    public string Link { get; set; } = "Link";
+    public string Guid { get; set; } = "Guid";
+
+    public override string ToString()
+    {
+        return $"Title: {Title}\nDescription: {Description}\nLink: {Link}\nGuid: {Guid}";
     }
 }
