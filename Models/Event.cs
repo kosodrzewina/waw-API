52e04e890af0e1556b16604695ebacf23d0f65d6
Piotr Sienkiewicz <sienkiew@live.com>
2022-01-09 01:07:33 +0100

    model for events

---
+++ Models/Event.cs
@@ -0,0 +1,10 @@
+﻿namespace WawAPI.Models
+{
+    public class Event
+    {
+        public string Title { get; set; } = "Title";
+        public string Description { get; set; } = "Description";
+        public string Link { get; set; } = "Link";
+        public string Guid { get; set; } = "Guid";
+    }
+}
