061cee2ecc87c2e384e768739c014eb41583ecbb
Piotr Sienkiewicz <sienkiew@live.com>
2022-02-21 21:04:35 +0100

    db context

---
+++ Models/MainDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WawAPI.Configurations;
+
+namespace WawAPI.Models;
+public class MainDbContext : DbContext
+{
+    public DbSet<Event> Events { get; set; }
+
+    public MainDbContext() {}
+
+    public MainDbContext(DbContextOptions options) : base(options) {}
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlServer("Server=(LocalDB)\\WawServer; Database=WawDB");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfiguration(new EventTypeConfiguration());
+    }
+}
