e6730f4c1257fa62504b40e83615aa4a46a386e0
Piotr Sienkiewicz <sienkiew@live.com>
2022-03-01 21:01:03 +0100

    db service and dto for events

---
+++ DTOs/EventDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WawAPI.DTOs;
+
+public class EventDto
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Link { get; set; }
+    public string Type { get; set; }
+}
---
+++ Services/MainDbService.cs
@@ -0,0 +1,47 @@
+﻿using WawAPI.DTOs;
+using WawAPI.Models;
+
+namespace WawAPI.Services;
+
+public interface IDatabaseService
+{
+    public EventDto? GetEvent(string guid);
+}
+
+public class MainDbService : IDatabaseService
+{
+    private readonly MainDbContext _context = new();
+
+    public EventDto? GetEvent(string guid)
+    {
+        var query = _context.Events
+            .Where(e => e.Guid.Equals(guid))
+            .Join(
+                _context.EventTypes,
+                e => e.IdEventType,
+                t => t.Id,
+                (e, t) => new
+                {
+                    e.Title,
+                    e.Description,
+                    e.Link,
+                    EventTypeName = t.Name
+                }
+            );
+
+        if (query.Any())
+        {
+            var @event = query.First();
+
+            return new EventDto
+            {
+                Title = @event.Title,
+                Description = @event.Description,
+                Link = @event.Link,
+                Type = @event.EventTypeName
+            };
+        }
+
+        return null;
+    }
+}
